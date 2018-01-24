d32cf7984cf6bf9b744084689d4d2e6a05de909b
danny201753 <dannylovewargame@gmail.com>
2018-01-24 09:26:17 -0400

    made change

--- machine_learning_assignment_1.py
+++ machine_learning_assignment_1.py
@@ -2,6 +2,7 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
 from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 
 # this is the data for training purposes
@@ -19,13 +20,13 @@ X_train, X_test, y_train, y_test =train_test_split(
 
 # choosing svm for classification
 # model =SVC(kernel ='linear', C =1).fit(X_train, y_train)
-model =SVC(kernel='rbf', C=1.2).fit(X_train, y_train)
-print(y_test.shape, '\n', y_train.shape)
-# prediction from testing data 
+# model =SVC(kernel='rbf', C=1.2).fit(X_train, y_train)
+model =RandomForestClassifier(n_estimators =10).fit(X_train, y_train)
 svm_predictions =model.predict(X_test)
 
 # model accuracy for X_test
 accuracy =model.score(X_test, y_test)
 print(accuracy)
+
 
 
