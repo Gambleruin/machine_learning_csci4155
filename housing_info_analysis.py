b268fcba694c61d2fcec873076734c2ef37a5e4d
Gambleruin <dannylovewargame@gmail.com>
2018-02-05 09:48:34 -0400

    *

--- housing_info_analysis.py
+++ housing_info_analysis.py
@@ -1,3 +1,4 @@
+
 import random
 import numpy as np
 import pandas as pd
@@ -37,13 +38,11 @@ print(type(y1), type(y2))
 '''
 TBF6D:Desktop daniel$ python3 housing_info_analysis.py 
 (9,) (21613,) 
-
 (9,) (21613,)
 '''
 '''
 # initial theta
 a=np.array([-1]); b=np.array([-1]); L=np.array([])
-
 for iter in range(10-1):
     y_hat=a[-1]*X_2+b[-1]
     a=np.append(a,a[-1]-lr*sum((y_hat-y2)*X_2))
@@ -77,8 +76,14 @@ def gradient_descent(alpha, x, y, ep, max_iter=100000):
         grad1 = 1.0/m * sum([(t0 + t1*x[i] - y[i])*x[i] for i in range(m)])
 
         # update the theta_temp
+        
         temp0 = t0 - alpha * grad0
         temp1 = t1 - alpha * grad1
+        
+        '''
+        temp0 =t0*(1-2*5*alpha) -alpha*grad0
+        temp1 =t1*(1-2*5*alpha) -alpha*grad1
+        '''
     
         # update theta
         t0 = temp0
@@ -99,24 +104,23 @@ def gradient_descent(alpha, x, y, ep, max_iter=100000):
 
     return t0,t1
 
-lr=100
+lr=0.000001
 ep =0.01
 theta0, theta1 =gradient_descent(lr, std_data, y2, ep, max_iter =100)
 print (theta0, theta1)
 
-y_hat =theta1*std_data +theta0 
+y_hat =theta1*X_2 +theta0 
 
-slope, intercept, r_value, p_value, std_err = stats.linregress(std_data,y2)
-
-
-plt.xlim([min(std_data),max(std_data)])
+'''
+plt.xlim([min(X_2),max(X_2)])
 plt.ylim([min(y2),max(y2)])
+'''
 print(y2, '\n', y_hat)
-plt.plot(std_data, y2, '*')
-plt.plot(std_data, y_hat)
-plt.plot([min(std_data),max(std_data)],[intercept+slope*min(std_data),intercept+slope*max(std_data)])
-# plt.xlabel('x'); plt.ylabel('y')
+plt.plot(X_2, y2, '*')
+plt.plot(X_2, y_hat)
+
 plt.show()
+
 
 
 
