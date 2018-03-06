8dd7956cd0819c91251685e494d0fdbcd7daa05c
Gambleruin <dannylovewargame@gmail.com>
2018-03-06 18:53:53 -0400

    *

--- multi_layer perceptron.py
+++ multi_layer perceptron.py
@@ -1,7 +1,25 @@
-# multi_layer perceptron
+# multi_layer_perceptron
+import csv
 import numpy as np 
+import pandas as pd
 import matplotlib.pyplot as plt 
+'''
+with open('pattern1.csv') as f:
+	for line in f:
+		print(type(line.rstrip()))
+'''
+count =0
+# the column header will never be needed
+num_instance =26
+df =pd.read_csv('pattern1.csv', names =[' '])
+# print (df.iloc[0: 12])
+for i in range(num_instance):
+	training_instance =df.iloc[count: count+12].values
+	print ('this is ', i, 'th traning instance\n', training_instance, '\n')
+	count =count +12
 
+
+# design Multiple Layer perceptron to read in ndarray as training instance
 X=np.array([[0,0,1,1],
 	[0,1,0,1],
 	[1,1,1,1]])
@@ -11,7 +29,7 @@ Y =np.array([[1,0,0,1]])
 Ni =3; Nh =4; No =1;
 
 # parameter and array initialization
-Ntrials =1000
+Ntrials =10000
 wh =np.random.randn(Nh, Ni); dwh =np.zeros(wh.shape)
 wo =np.random.randn(No, Nh); dwo =np.zeros(wo.shape)
 error =np.array([])
@@ -19,6 +37,8 @@ error =np.array([])
 for trial in range(Ntrials):
 	h =1/(1+np.exp(-wh@X)) #hidden activation for all pattern
 	y =1/(1+np.exp(-wo@h)) #output for all pattern
+	#print(y)
+	#break
 
 	do =y*(1-y)*(Y-y) #delta output
 	dh =h*(1-h)*(wo.transpose()@do) #delta backpropagated 
@@ -30,6 +50,7 @@ for trial in range(Ntrials):
 	wh=wh+0.1*dwh
 
 	error =np.append(error, np.sum(abs(Y-y)))
+
 
 plt.plot(error)
 plt.show()
