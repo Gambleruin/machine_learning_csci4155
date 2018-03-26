ca9805c4a6b6b77a708c365bb47c59f64dc638be
Gambleruin <dannylovewargame@gmail.com>
2018-03-26 02:35:25 -0300

    *

---
+++ cnn_mnist_conv_layers_diff_config.py
@@ -0,0 +1,145 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+tf.logging.set_verbosity(tf.logging.INFO)
+def layer_config_mod0(features):
+  input_layer = tf.reshape(features["x"], [-1, 28, 28, 1])
+  conv = tf.layers.conv2d(
+      inputs=input_layer,
+      filters=32,
+      kernel_size=[5, 5],
+      padding="same",
+      activation=tf.nn.relu)
+  pool = tf.layers.max_pooling2d(inputs=conv, pool_size=[2, 2], strides=2)
+
+  pool_flat = tf.reshape(pool, [-1, 14 * 14 * 32])
+  dense = tf.layers.dense(inputs=pool_flat, units=1024, activation=tf.nn.relu)
+  return dense
+
+def layer_config_mod1(features):
+  input_layer = tf.reshape(features["x"], [-1, 28, 28, 1])
+  conv1 = tf.layers.conv2d(
+      inputs=input_layer,
+      filters=32,
+      kernel_size=[5, 5],
+      padding="same",
+      activation=tf.nn.relu)
+  pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
+  conv2 = tf.layers.conv2d(
+      inputs=pool1,
+      filters=64,
+      kernel_size=[5, 5],
+      padding="same",
+      activation=tf.nn.relu)
+
+  pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
+  pool2_flat = tf.reshape(pool2, [-1, 7 * 7 * 64])
+  dense = tf.layers.dense(inputs=pool2_flat, units=1024, activation=tf.nn.relu)
+  return dense
+
+def layer_config_mod2(features):
+  input_layer = tf.reshape(features["x"], [-1, 28, 28, 1])
+  conv1 = tf.layers.conv2d(
+      inputs=input_layer,
+      filters=32,
+      kernel_size=[5, 5],
+      padding="same",
+      activation=tf.nn.relu)
+  pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
+  conv2 = tf.layers.conv2d(
+      inputs=pool1,
+      filters=64,
+      kernel_size=[5, 5],
+      padding="same",
+      activation=tf.nn.relu)
+
+  pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
+  conv3 = tf.layers.conv2d(
+      inputs=pool2,
+      filters=128,
+      kernel_size=[5, 5],
+      padding="same",
+      activation=tf.nn.relu)
+
+  pool3 = tf.layers.max_pooling2d(inputs=conv3, pool_size=[3, 3], strides=2)
+  print('this is pooled 3', np.shape(pool3))
+  pool3_flat = tf.reshape(pool3, [-1, 3 * 3 * 128])
+  dense = tf.layers.dense(inputs=pool3_flat, units=1024, activation=tf.nn.relu)
+  return dense
+
+def cnn_model_fn(features, labels, mode):
+  # mod0 is one layer, mod1 is (default) 2 layers, mod2 is 3 layers
+  dense = layer_config_mod0(features)
+  print(np.shape(dense))
+  print('\n\n\n\n\n\n\n\n\n\n\n\n\n')
+  dropout = tf.layers.dropout(
+      inputs=dense, rate=0.4, training=mode == tf.estimator.ModeKeys.TRAIN)
+  logits = tf.layers.dense(inputs=dropout, units=10)
+
+  predictions = {
+      "classes": tf.argmax(input=logits, axis=1),
+      "probabilities": tf.nn.softmax(logits, name="softmax_tensor")
+  }
+
+  if mode == tf.estimator.ModeKeys.PREDICT:
+    return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
+  loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
+  if mode == tf.estimator.ModeKeys.TRAIN:
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
+    train_op = optimizer.minimize(
+        loss=loss,
+        global_step=tf.train.get_global_step())
+    return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
+  eval_metric_ops = {
+      "accuracy": tf.metrics.accuracy(
+          labels=labels, predictions=predictions["classes"])}
+  return tf.estimator.EstimatorSpec(
+      mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
+
+
+def main(unused_argv):
+  mnist = tf.contrib.learn.datasets.load_dataset("mnist")
+  print("Size of:")
+  print("- Training-set:\t\t{}".format(len(mnist.train.labels)))
+  print("- Test-set:\t\t{}".format(len(mnist.test.labels)))
+  print("- Validation-set:\t{}".format(len(mnist.validation.labels)))
+  
+  train_data = mnist.train.images  # Returns np.array
+  train_labels = np.asarray(mnist.train.labels, dtype=np.int32)
+  eval_data = mnist.test.images  # Returns np.array
+  eval_labels = np.asarray(mnist.test.labels, dtype=np.int32)
+
+  mnist_classifier = tf.estimator.Estimator(
+      model_fn=cnn_model_fn, model_dir="/tmp/mnist_convnet_model")
+    
+  tensors_to_log = {"probabilities": "softmax_tensor"}
+  logging_hook = tf.train.LoggingTensorHook(
+      tensors=tensors_to_log, every_n_iter=10)
+
+  train_input_fn = tf.estimator.inputs.numpy_input_fn(
+      x={"x": train_data},
+      y=train_labels,
+
+      batch_size=5,
+      num_epochs=None,
+      shuffle=True)
+  mnist_classifier.train(
+      input_fn=train_input_fn,
+      steps=20000,
+      hooks=[logging_hook])
+
+  eval_input_fn = tf.estimator.inputs.numpy_input_fn(
+      x={"x": eval_data},
+      y=eval_labels,
+      num_epochs=1,
+      shuffle=False)
+  eval_results = mnist_classifier.evaluate(input_fn=eval_input_fn)
+  print(eval_results)
+
+if __name__ == "__main__":
+  tf.app.run()
